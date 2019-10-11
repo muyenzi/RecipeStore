6b9b9564faa995b9cee219f1e863c70a34a1c793
Muyenzi Raissa <muyenziraissa@gmail.com>
2019-10-11 17:20:37 +0200

    added the background image and the search button

--- app/src/main/java/com/moringaschool/recipestore/MainActivity.java
+++ app/src/main/java/com/moringaschool/recipestore/MainActivity.java
@@ -3,12 +3,25 @@ package com.moringaschool.recipestore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSearch =(Button)findViewById(R.id.search_button);
+
+        mSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Search!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
