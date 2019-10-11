04f44bd9089b9ac4e068a8a5923093000b16deb7
Muyenzi Raissa <muyenziraissa@gmail.com>
2019-10-11 17:56:23 +0200

    added recipes button

--- app/src/main/java/com/moringaschool/recipestore/MainActivity.java
+++ app/src/main/java/com/moringaschool/recipestore/MainActivity.java
@@ -2,25 +2,28 @@ package com.moringaschool.recipestore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button mSearch;
+    private Button mRecipes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSearch =(Button)findViewById(R.id.search_button);
+        mRecipes =(Button)findViewById(R.id.recipes);
 
-        mSearch.setOnClickListener(new View.OnClickListener() {
+        mRecipes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Search!", Toast.LENGTH_LONG).show();
+//                Toast.makeText(MainActivity.this, "Search!", Toast.LENGTH_LONG).show();
+                Intent intent=new Intent(MainActivity.this,menuActivity.class);
+                startActivity(intent);
             }
         });
     }
