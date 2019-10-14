d191bd423dfed85acdccfa0b4518d743c60b23d1
Muyenzi Raissa <muyenziraissa@gmail.com>
2019-10-14 09:25:38 +0200

    The sisth image description

--- app/src/main/java/com/moringaschool/recipestore/Images6Activity.java
+++ app/src/main/java/com/moringaschool/recipestore/Images6Activity.java
@@ -18,13 +18,13 @@ public class Images6Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_images6);
-//        ButterKnife.bind(this);
-//
-//        Intent images6 = getIntent();
-//
-//        mMenu6TextView.setText("\n*Ingredients\n\n" +
-//                "Butter ," + "White sugar ," + "Brown sugar ," + "Eggs ," + "Vanilla \n" + "Flour ," + "Baking soda ," + "Salt ," + "Chocolate chips ," + "Oatmeal" + "-Coconut \n\n" +
-//                "* Directions\n" + "1.Preheat the oven to 325.\n" + "2.Beat the butter, sugar, eggs and vanilla together until creamy.\n" + "3.Mix together the flour, baking soda and salt in a separate bowl.\n" +
-//                "4.Add flour mixture to butter mixture slowly.\n" + "5.Stir in chocolate chips, oatmeal and coconut.\n" + "6.Bake for 10 minutes or until golden brown.");
+        ButterKnife.bind(this);
+
+        Intent images6 = getIntent();
+
+        mMenu6TextView.setText("\n*Ingredients\n\n" +
+                "Butter ," + "White sugar ," + "Brown sugar ," + "Eggs ," + "Vanilla \n" + "Flour ," + "Baking soda ," + "Salt ," + "Chocolate chips ," + "Oatmeal" + "-Coconut \n\n" +
+                "* Directions\n" + "1.Preheat the oven to 325.\n" + "2.Beat the butter, sugar, eggs and vanilla together until creamy.\n" + "3.Mix together the flour, baking soda and salt in a separate bowl.\n" +
+                "4.Add flour mixture to butter mixture slowly.\n" + "5.Stir in chocolate chips, oatmeal and coconut.\n" + "6.Bake for 10 minutes or until golden brown.");
     }
         }
