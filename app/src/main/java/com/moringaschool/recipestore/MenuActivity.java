ac56832ef4cf4f841ad877c58a747aca48339216
Muyenzi Raissa <muyenziraissa@gmail.com>
2019-10-26 13:34:43 +0200

    added the preferences

--- app/src/main/java/com/moringaschool/recipestore/MenuActivity.java
+++ app/src/main/java/com/moringaschool/recipestore/MenuActivity.java
@@ -14,7 +14,6 @@ public class MenuActivity extends AppCompatActivity {
 
     @BindView(R.id.menu1TextView) TextView mMenu1TextView;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +22,6 @@ public class MenuActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         Intent images1=getIntent();
-
         mMenu1TextView.setText("\n*Ingredients\n\n" +
                 "Butter ," + "White sugar ," + "Brown sugar ," + "Eggs ," + "Vanilla \n" + "Flour ," + "Baking soda ," + "Salt ," + "Chocolate chips ," + "Oatmeal" + "-Coconut \n\n"+
                 "* Directions\n" + "1.Preheat the oven to 325.\n" + "2.Beat the butter, sugar, eggs and vanilla together until creamy.\n" + "3.Mix together the flour, baking soda and salt in a separate bowl.\n" +
