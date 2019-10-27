7606b2cfdb446f10a0ec494da34a42520d36d7c1
Muyenzi Raissa <muyenziraissa@gmail.com>
2019-10-27 13:27:48 +0200

    created the logout

--- app/src/main/java/com/moringaschool/recipestore/MainActivity.java
+++ app/src/main/java/com/moringaschool/recipestore/MainActivity.java
@@ -5,11 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.service.autofill.Validator;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 //import butterknife.Bind;
+import com.google.firebase.auth.FirebaseAuth;
+import com.moringaschool.recipestore.ui.LoginActivity;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -34,6 +40,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     startActivity(intent);
                 }
             }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.logout, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.userLogout){
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout(){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
         }
 
 
